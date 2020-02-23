c4ded4d2a2fc57fdaf7e3144cfa187585990240e
Minh Phuoc <tranminhphuoc@gmail.com>
2020-02-23 17:39:30 +0700

    Add router

--- hello-aurelia/src/app.ts
+++ hello-aurelia/src/app.ts
@@ -1,13 +1,28 @@
+import { inject } from 'aurelia-dependency-injection';
 import { TodoList } from "todo-list";
+import {EventAggregator} from "aurelia-event-aggregator";
 
 export class App {
   heading = 'Todo';
   description: HTMLInputElement;
   list: TodoList;
 
+  static inject = [EventAggregator]
+
+  constructor(public ee: EventAggregator) {
+
+  }
+
   addTodo() {
-    this.list.add(this.description.value);
+    this.ee.publish('todo:add', this.description.value);
     this.description.value = '';
+  }
+
+  configureRouter(config) {
+    config.map([
+      { route: '', redirect: 'todos/all'},
+      { route: 'todos/:filter', moduleId: 'todo-list'},
+    ])
   }
 
 }
--- hello-aurelia/src/todo-list.ts
+++ hello-aurelia/src/todo-list.ts
@@ -1,7 +1,28 @@
+import { EventAggregator } from 'aurelia-event-aggregator';
 import { Todo } from "todo";
 
 export class TodoList {
   todos: Todo[] = [];
+  todosView: Todo[] = [];
+  subscription;
+  filter: any;
+
+  static inject = [EventAggregator]
+
+  constructor(public ee: EventAggregator) {
+    this.subscription = this.ee.subscribe('todo:add', d => this.add(d));
+    this.filter = this.filters.all;
+    this.invalidateView();
+  }
+
+  invalidateView() {
+    this.todosView = this.filter(this.todos);
+  }
+
+  activate(params, routeConfig, navigationInstruction) {
+    this.filter = this.filters[params.filter];
+    this.invalidateView();
+  }
 
   add(descriptionText) {
     if (descriptionText) {
@@ -11,5 +32,17 @@ export class TodoList {
 
   remove(todo) {
     todo.remove();
+  }
+
+  filters = {
+    all(todos) {
+      return todos;
+    },
+    active(todos) {
+      return todos.filter(x => !x.done);
+    },
+    completed(todos) {
+      return todos.filter(x => x.done);
+    }
   }
 }
